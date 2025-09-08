62e9ba6268450df2a96e775fe98901586de7ffa4
kmalone <kmalone@granadainsurance.com>
2025-09-08 16:05:32 -0400

    added lsps to lsp-setup.lua

--- lua/plugins/lsp-setup.lua
+++ lua/plugins/lsp-setup.lua
@@ -1,6 +1,13 @@
 require('mason').setup()
 require('mason-lspconfig').setup()
 
+-- how to edit: 
+-- add lsp server name (NOT ANSIBLE PACKAGE)
+-- :help mason.nvim
+-- :lua print(vim.inspect(require("mason-lspconfig.mappings.server").package_to_lspconfig))
+-- not working
+-- :lua print(vim.inspect(require("mason-lspconfig.mappings.server").ansible-language-server))
+
 local servers = {
     lua_ls = {
         Lua = {
@@ -9,6 +16,17 @@ local servers = {
             telemetry = { enable = false },
         },
     },
+    basedpyright = {},
+    ruff = {},
+    bashls = {},
+    ast_grep = {},
+    ansiblels = {},
+    cssls = {},
+    awk_ls = {},
+    systemd_ls = {},
+    --perl-debug-adapter = {},
+    perlnavigator = {},
+    codebook = {},
 }
 
 vim.lsp.enable('glsl_analyzer')
