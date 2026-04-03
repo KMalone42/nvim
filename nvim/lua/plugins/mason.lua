c6f42adba13ed4d1e91d25d5739a95e1eac06aac
kmalone42 <kmalone42@outlook.com>
2026-04-03 19:13:56 -0400

    cleaned up the mason config

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,15 @@
+-- lua/plugins/mason.lua
+
+-- mason-lspconfig is a package that allows me to declare lsps in config/lsp.lua
+-- lsp.lua names come from `:h lspconfig-all`
+-- this takes those names and downloads corresponding mason packages
+return {
+  "mason-org/mason-lspconfig.nvim",
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+  opts = {
+    ensure_installed = { "lua_ls", "basedpyright", "rust_analyzer", "ts_ls" },
+  },
+}
